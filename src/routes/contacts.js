d16da49686aaf6d695e3af1e743374280b1c3c32
Yusuf Soylu <yusufsoylu1974@gmail.com>
2026-04-12 18:58:53 +0300

    main-empty

--- src/routes/contacts.js
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import{getAllContactsController,getContactByIdController} from "../controllers/contacts.js";
-
-
-const router=Router();
-router.get("/",getAllContactsController);
-  router.get("/:contactId",getContactByIdController);
-  export default router;  
