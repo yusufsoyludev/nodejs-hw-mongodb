bf6c9fdbc705a48ecc2ea673a70edcb171be7e94
Yusuf Soylu <yusufsoylu1974@gmail.com>
2026-04-19 18:40:15 +0300

    swagger-123

--- src/server.js
+++ src/server.js
@@ -1,24 +1,40 @@
 import express from 'express';
 import cors from 'cors';
 import pinoHttp from 'pino-http';
+import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
 import contactsRouter from './routes/contacts.js';
 import authRouter from './routes/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import cookieParser from 'cookie-parser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerPath = path.join(__dirname, '../docs/swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf-8'));
 
 export const setupServer = () => {
   const app = express();
   const PORT = process.env.PORT || 3000;
 
-  app.use(cors({
+  app.use(
+    cors({
       origin: true,
       credentials: true,
     }),
   );
+
   app.use(pinoHttp());
   app.use(express.json());
   app.use(cookieParser());
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
